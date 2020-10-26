271f89d49e3b185cae48d21a77dd030429a99184
Roman <rnbelov96@gmail.com>
2020-10-26 07:17:12 +0300

    Устанавливает и настраивает axios

---
+++ src/express/api.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const axios = require(`axios`);
+const {DEFAULT_API_PORT} = require(`../constants`);
+
+class API {
+
+  constructor(baseURL, timeout) {
+    this._http = axios.create({
+      baseURL,
+      timeout
+    });
+  }
+
+  async _load(url, options) {
+    const response = await this._http.request({url, ...options});
+    return response.data;
+  }
+
+  getArticles() {
+    return this._load(`/articles`);
+  }
+
+  getArticle(id) {
+    return this._load(`/articles/${id}`);
+  }
+
+  search(query) {
+    return this._load(`/search`, {params: {query}});
+  }
+
+  getCategories() {
+    return this._load(`/category`);
+  }
+
+  createArticle(data) {
+    return this._load(`/articles`, {
+      method: `POST`,
+      data
+    });
+  }
+}
+
+const TIMEOUT = 2000;
+
+const port = process.env.API_PORT || DEFAULT_API_PORT;
+const defaultUrl = `http://localhost:${port}/api/`;
+
+const defaultAPI = new API(defaultUrl, TIMEOUT);
+
+module.exports = {
+  API,
+  getAPI: () => defaultAPI
+};
