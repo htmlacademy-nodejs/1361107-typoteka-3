9381af534a0a33acadbf629a395a12e605011aec
Belov Roman <rnbelov96@gmail.com>
2020-10-28 17:04:04 +0300

    Настраивает поиск

--- src/express/routes/search-routes.js
+++ src/express/routes/search-routes.js
@@ -1,9 +1,22 @@
 "use strict";
 
 const {Router} = require(`express`);
+const api = require(`../api`).getAPI();
 
 const searchRouter = new Router();
 
-searchRouter.get(`/`, (req, res) => res.render(`search`));
+searchRouter.get(`/`, async (req, res) => {
+  const {search} = req.query;
+  try {
+    const results = await api.search(search);
+    res.render(`search`, {
+      results, search
+    });
+  } catch (error) {
+    res.render(`search`, {
+      results: [], search
+    });
+  }
+});
 
 module.exports = searchRouter;
