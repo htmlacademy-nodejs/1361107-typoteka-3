18cd0752807c34861664f4dd0106df93ead2f855
Belov Roman <rnbelov96@gmail.com>
2020-10-29 16:58:01 +0300

    Исправляет функцию форматирования даты

--- src/utils.js
+++ src/utils.js
@@ -36,10 +36,17 @@ exports.readContent = async (fileName) => {
 
 exports.formatDate = (date) => {
   const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
-  const month = date.getMonth() < 10 ? `0${date.getMonth()}` : date.getMonth();
+  const month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
   const year = date.getFullYear();
   const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
   const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
 
   return `${day}.${month}.${year}, ${hours}:${minutes}`;
+};
+
+exports.getTime = (date) => {
+  const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
+  const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+
+  return `${hours}:${minutes}`;
 };
