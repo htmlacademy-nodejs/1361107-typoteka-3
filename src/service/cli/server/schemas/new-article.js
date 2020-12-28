ca03294d4ade28285ce7bac24232db717eb704da
Belov Roman <rnbelov96@gmail.com>
2020-12-28 05:49:20 +0300

    Обновляет схему валидации нового объявления

--- src/service/cli/server/schemas/new-article.js
+++ src/service/cli/server/schemas/new-article.js
@@ -4,28 +4,33 @@ const Joi = require(`joi`);
 const {NewArticleMessage} = require(`../../../../constants`);
 
 module.exports = Joi.object({
-  title: Joi.string().required().min(30).max(250).messages({
+  title: Joi.string().label(`Заголовок`).required().min(30).max(250).messages({
     "string.min": NewArticleMessage.MIN_TITLE_LENGTH,
     "string.max": NewArticleMessage.MAX_TITLE_LENGTH,
     "any.required": NewArticleMessage.REQUIRED_FIELD,
+    "string.empty": NewArticleMessage.REQUIRED_FIELD,
   }),
-  announce: Joi.string().required().min(30).max(250).messages({
+  announce: Joi.string().label(`Анонс`).required().min(30).max(250).messages({
     "string.min": NewArticleMessage.MIN_ANNOUNCE_LENGTH,
     "string.max": NewArticleMessage.MAX_ANNOUNCE_LENGTH,
     "any.required": NewArticleMessage.REQUIRED_FIELD,
+    "string.empty": NewArticleMessage.REQUIRED_FIELD,
   }),
-  fullText: Joi.string().max(1000).messages({
+  fullText: Joi.string().label(`Описание`).max(1000).messages({
     "string.max": NewArticleMessage.MAX_FULL_TEXT_LENGTH,
+    "string.empty": NewArticleMessage.REQUIRED_FIELD,
   }),
   picture: Joi.string(),
   userId: Joi.number()
+    .label(`ID пользователя`)
     .required()
     .messages({"any.required": NewArticleMessage.REQUIRED_FIELD}),
   categories: Joi.array()
+    .label(`Категории`)
     .items(Joi.number())
     .required()
     .messages({
       "any.required": NewArticleMessage.REQUIRED_FIELD,
-      "number.base": NewArticleMessage.WRONG_CATEGORY
+      "number.base": NewArticleMessage.WRONG_CATEGORY,
     }),
 });
